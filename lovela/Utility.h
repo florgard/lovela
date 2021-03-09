6adac4f3802ee2f6512d0666270d0aad7b1086cf
florgard <love_florgard@hotmail.com>
2021-03-09 17:24:51 +0100

    Adjust specifiers.

--- lovela/Utility.h
+++ lovela/Utility.h
@@ -6,14 +6,14 @@
 }
 
 template <typename Enum, typename std::enable_if<std::is_enum_v<Enum>>::type* = nullptr>
-[[nodiscard]] inline std::wstring to_wstring(Enum value)
+[[nodiscard]] std::wstring to_wstring(Enum value)
 {
 	return to_wstring(magic_enum::enum_name(value));
 }
 
 // https://www.reddit.com/r/cpp/comments/g05m1r/stdunique_ptr_and_braced_initialization/
 template <class T>
-std::unique_ptr<T> clone_unique(T t)
+[[nodiscard]] std::unique_ptr<T> clone_unique(T t)
 {
 	return std::make_unique<T>(std::move(t));
 }
