2315350ed76edd5c3bae99b55b7d0a613b007165
florgard <love_florgard@hotmail.com>
2021-03-09 17:18:13 +0100

    Add clone_unique helper.

--- lovela/Utility.h
+++ lovela/Utility.h
@@ -10,3 +10,10 @@ template <typename Enum, typename std::enable_if<std::is_enum_v<Enum>>::type* = 
 {
 	return ToWString(magic_enum::enum_name(value));
 }
+
+// https://www.reddit.com/r/cpp/comments/g05m1r/stdunique_ptr_and_braced_initialization/
+template <class T>
+std::unique_ptr<T> clone_unique(T t)
+{
+	return std::make_unique<T>(std::move(t));
+}
