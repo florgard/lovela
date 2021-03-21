3e77acd214931320eae92a8a17f814fc64fbfed7
florgard <love_florgard@hotmail.com>
2021-03-21 12:03:57 +0100

    Use inner function for the function body implementation to be able to deduce the function return type also on exceptions. Add error handling tests.

--- targets/cpp/tests/test.cpp
+++ targets/cpp/tests/test.cpp
@@ -1,15 +1,22 @@
 #include "pch.h"
 #include "test.h"
 
-auto f_success(lovela::context& context, const auto& in)
+auto f_ReturnInput(lovela::context& context, const auto& in)
 {
 	auto& v1 = in; v1; context;
 	return v1;
 }
 
+auto f_ReturnInputIncremented(lovela::context& context, const auto& in)
+{
+	auto& v1 = in; v1; context;
+	auto v2 = v1 + 1;
+	return v2;
+}
+
 #pragma warning(push)
 #pragma warning(disable: 4702)
-auto f_error(lovela::context& context, const auto& in)
+auto f_RaisesError(lovela::context& context, const auto& in)
 {
 	auto& v1 = in; v1; context;
 	throw lovela::error("error");
@@ -17,45 +24,82 @@ auto f_error(lovela::context& context, const auto& in)
 }
 #pragma warning(pop)
 
-auto f_error_handler(lovela::context& context, const auto& in)
+auto f_ErrorHandlerReset(lovela::context& context, const auto& in)
 {
 	auto& v1 = in; v1; context;
-	auto v2 = 0;
-	return v2;
+	return v1;
 }
 
-auto f_inner_error(lovela::context& context, const auto& in)
+auto fb_WithErrorHandlerError(lovela::context& context, const auto& in)
 {
 	auto& v1 = in; v1; context;
+	auto v2 = 456;
+	auto v3 = f_RaisesError(context, v2);
+	return v3;
+}
+
+auto f_WithErrorHandlerError(lovela::context& context, const auto& in)
+{
+	decltype(fb_WithErrorHandlerError(context, in)) out;
 
 	try
 	{
-		auto v2 = f_error(context, v1);
-		return v2;
+		out = fb_WithErrorHandlerError(context, in);
 	}
 	catch (const lovela::error& error)
 	{
 		context.error = error;
-		auto v3 = f_error_handler(context, in);
-		return v3;
+		out = f_ErrorHandlerReset(context, in);
 	}
-	catch (const std::exception&)
+
+	return out;
+}
+
+auto fb_WithErrorHandlerNoError(lovela::context& context, const auto& in)
+{
+	auto& v1 = in; v1; context;
+	auto v2 = 456;
+	return v2;
+}
+
+auto f_WithErrorHandlerNoError(lovela::context& context, const auto& in)
+{
+	decltype(fb_WithErrorHandlerNoError(context, in)) out;
+
+	try
 	{
-		throw;
+		out = fb_WithErrorHandlerNoError(context, in);
 	}
+	catch (const lovela::error& error)
+	{
+		context.error = error;
+		out = f_ErrorHandlerReset(context, in);
+	}
+
+	return out;
 }
 
-TEST(ReturnValueTest, Success) {
+TEST(ReturnValueTest, ReturnInput) {
 	lovela::context context;
-	EXPECT_EQ(f_success(context, 123), 123);
+	EXPECT_EQ(f_ReturnInput(context, 123), 123);
 }
 
-TEST(ReturnValueTest, UnhandledError) {
+TEST(ReturnValueTest, ReturnInputIncremented) {
 	lovela::context context;
-	EXPECT_THROW(f_error(context, 123), lovela::error);
+	EXPECT_EQ(f_ReturnInputIncremented(context, 123), 124);
 }
 
-TEST(ReturnValueTest, HandledError) {
+TEST(ReturnValueTest, RaisesError) {
 	lovela::context context;
-	EXPECT_EQ(f_inner_error(context, 123), 0);
+	EXPECT_THROW(f_RaisesError(context, 123), lovela::error);
+}
+
+TEST(ReturnValueTest, WithErrorHandlerError) {
+	lovela::context context;
+	EXPECT_EQ(f_WithErrorHandlerError(context, 123), 123);
+}
+
+TEST(ReturnValueTest, WithErrorHandlerNoError) {
+	lovela::context context;
+	EXPECT_EQ(f_WithErrorHandlerNoError(context, 123), 456);
 }
