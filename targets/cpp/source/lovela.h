5c8a5ae403da8f7c62e87d204274ec25574eed55
florgard <love_florgard@hotmail.com>
2021-04-07 17:08:08 +0200

    Prepare for better stream support.

--- targets/cpp/source/lovela.h
+++ targets/cpp/source/lovela.h
@@ -6,14 +6,125 @@
 #include <exception>
 #include <typeinfo>
 #include <cstdint>
+#include <variant>
+#include <algorithm>
+#include "utfcpp/utf8.h"
 
 namespace lovela
 {
+	template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
+	template<class... Ts> overloaded(Ts...)->overloaded<Ts...>;
+
+	class stream
+	{
+		std::variant<std::monostate, std::istream*, std::ostream*, std::wistream*, std::wostream*> io;
+		std::wstring buffer;
+		
+		static void to_string(std::string& str, std::wstring_view value)
+		{
+			str.resize(value.length());
+			std::transform(value.begin(), value.end(), str.begin(), [](auto elem) { return static_cast<char>(elem); });
+		}
+
+		static void to_wstring(std::wstring& str, std::string_view value)
+		{
+			str.resize(value.length());
+
+			if constexpr (sizeof(wchar_t) == 2)
+			{
+				utf8::utf8to16(value.begin(), value.end(), str.begin());
+			}
+			else
+			{
+				utf8::utf8to32(value.begin(), value.end(), str.begin());
+			}
+		}
+
+	public:
+		stream() noexcept = default;
+		~stream() noexcept = default;
+		stream(std::istream& stream) noexcept : io(&stream) {}
+		stream(std::ostream& stream) noexcept : io(&stream) {}
+		stream(std::wistream& stream) noexcept : io(&stream) { buffer.reserve(4096); }
+		stream(std::wostream& stream) noexcept : io(&stream) { buffer.reserve(4096); }
+		stream(const stream& src) = delete;
+		stream(stream&& src) = delete;
+		stream& operator=(const stream& src) = delete;
+		stream& operator=(stream&& src) = delete;
+
+		void read_word(std::string& word)
+		{
+			std::visit(overloaded{
+				[&](auto&) {},
+				[&](std::istream* stream) { *stream >> word; },
+				[&](std::wistream* stream) { *stream >> buffer; to_string(word, buffer); }
+				}, io);
+		}
+
+		void read_line(std::string& line)
+		{
+			std::visit(overloaded{
+				[&](auto&) {},
+				[&](std::istream* stream) { std::getline(*stream, line); },
+				[&](std::wistream* stream) { std::getline(*stream, buffer); to_string(line, buffer); }
+				}, io);
+		}
+
+		void read_all(std::string& text)
+		{
+			std::visit(overloaded{
+				[&](auto&) {},
+				[&](std::istream* stream) { text.assign(std::istreambuf_iterator<char>(*stream), {}); },
+				[&](std::wistream* stream) { buffer.assign(std::istreambuf_iterator<wchar_t>(*stream), {}); to_string(text, buffer); }
+				}, io);
+		}
+
+		void read_bytes(std::string& bytes, size_t count)
+		{
+			std::visit(overloaded{
+				[&](auto&) {},
+				[&](std::istream* stream) { bytes.resize(count); stream->read(&bytes[0], count); }
+				}, io);
+		}
+
+		void write(std::string_view text)
+		{
+			std::visit(overloaded{
+				[&](auto) {},
+				[&](std::ostream* stream) { *stream << text; },
+				[&](std::wostream* stream) { to_wstring(buffer, text); *stream << buffer; }
+				}, io);
+		}
+
+		void write_line(std::string_view line)
+		{
+			std::visit(overloaded{
+				[&](auto&) {},
+				[&](std::ostream* stream) { *stream << line << '\n'; },
+				[&](std::wostream* stream) { to_wstring(buffer, line); *stream << buffer << '\n'; }
+				}, io);
+		}
+
+		void write_bytes(std::string_view bytes, size_t count)
+		{
+			std::visit(overloaded{
+				[&](auto&) {},
+				[&](std::ostream* stream) { stream->write(bytes.data(), count); }
+				}, io);
+		}
+	};
+
 	struct streams
 	{
-		std::wistream& in = std::wcin;
-		std::wostream& out = std::wcout;
-		std::wostream& err = std::wcerr;
+#if defined(_MSC_VER)
+		stream in{ std::wcin };
+		stream out{ std::wcout };
+		stream err{ std::wcerr };
+#else
+		stream in{ std::cin };
+		stream out{ std::cout };
+		stream err{ std::cerr };
+#endif
 
 		template <int index> auto& select() { static_assert(false, "invalid select index"); return *this; };
 		template <> auto& select<1>() { return in; }
