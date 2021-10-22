b570f3c00843d574a18e4731b4e2d7b88be517cf
florgard <love_florgard@hotmail.com>
2021-10-22 16:28:13 +0200

    Encapsulate.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -68,14 +68,15 @@ suite tokens = [] {
 	};
 };
 
-static constexpr std::array<std::pair<int, double>, 2> values{ { {1, 1.1}, {2, 2.2} } };
-static constexpr auto map = StaticMap<int, double, values.size()>{ {values} };
-static_assert(map.at(1) == 1.1);
-static_assert(map.at(2) == 2.2);
-static_assert(map.at_or(2, 1.1) == 2.2);
-static_assert(map.at_or(3, 1.1) == 1.1);
-
 suite static_map = [] {
+	static constexpr std::array<std::pair<int, double>, 2> values{ { {1, 1.1}, {2, 2.2} } };
+	static constexpr auto map = StaticMap<int, double, values.size()>{ {values} };
+
+	static_assert(map.at(1) == 1.1);
+	static_assert(map.at(2) == 2.2);
+	static_assert(map.at_or(2, 1.1) == 2.2);
+	static_assert(map.at_or(3, 1.1) == 1.1);
+
 	"Out of bounds"_test = [] {
 		expect(throws([] { if (map.at(3) == 3.3) {} }));
 	};
