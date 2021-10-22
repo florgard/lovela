d445bb0db4fac714a17377942d6a6be64fd98ee7
florgard <love_florgard@hotmail.com>
2021-10-22 17:25:52 +0200

    Separate test suites.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -18,56 +18,6 @@ import <regex>;
 
 using namespace boost::ut;
 
-suite tokens = [] {
-	"ParenRoundOpen type"_test = [] {
-		expect(LexerBase::GetTokenType('(') == Token::Type::ParenRoundOpen);
-	};
-
-	"SeparatorDot type"_test = [] {
-		expect(LexerBase::GetTokenType('.') == Token::Type::SeparatorDot);
-	};
-
-	"Empty type"_test = [] {
-		expect(LexerBase::GetTokenType(' ') == Token::Type::Empty);
-	};
-
-	"Compare empty"_test = [] {
-		expect(Token{} == Token{});
-	};
-
-	"Non-empty type"_test = [] {
-		expect(Token{ Token::Type::Identifier } != Token{});
-	};
-
-	"Named empty"_test = [] {
-		expect(Token{ {}, L"a" } != Token{});
-	};
-
-	"Equals"_test = [] {
-		expect(Token{ Token::Type::Identifier, L"a" } == Token{ Token::Type::Identifier, L"a" });
-	};
-
-	"Unequal type"_test = [] {
-		expect(Token{ Token::Type::Identifier, L"a" } != Token{ Token::Type::LiteralString, L"a" });
-	};
-
-	"Unequal name"_test = [] {
-		expect(Token{ Token::Type::Identifier, L"a" } != Token{ Token::Type::Identifier, L"b" });
-	};
-
-	"Compare empty to false"_test = [] {
-		expect(Token{} == false);
-	};
-
-	"Compare named empty to false"_test = [] {
-		expect(Token{ {}, L"a" } == false);
-	};
-
-	"Compare non-empty to true"_test = [] {
-		expect(Token{ Token::Type::Identifier } == true);
-	};
-};
-
 suite static_map = [] {
 	static constexpr std::array<std::pair<int, double>, 2> values{ { {1, 1.1}, {2, 2.2} } };
 	static constexpr auto map = StaticMap<int, double, values.size()>{ {values} };
