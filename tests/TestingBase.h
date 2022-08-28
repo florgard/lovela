d182fb0ee36c0be65a8d935ac086c34c6ed5bf26
Love Florgård <love_florgard@hotmail.com>
2022-08-28 17:40:05 +0200

    Let static functions be static.

--- tests/TestingBase.h
+++ tests/TestingBase.h
@@ -6,16 +6,16 @@ class TestingBase
 {
 public:
 	static bool TestLexer(const char* name, std::wstring_view code, const std::vector<Token>& expectedTokens, const std::vector<ILexer::Error>& expectedErrors = {});
-	std::unique_ptr<Node> TestParser(const char* name, std::wstring_view code, const Node& expectedTree, const std::vector<IParser::Error>& expectedErrors = {});
-	void TestCodeGenerator(const char* name, std::wstring_view code, std::wstring_view cppCode, int expectedErrors = 0);
-	void TestCodeGeneratorImport(const char* name, std::wstring_view code, std::wstring_view cppCode, int expectedErrors = 0);
-	void TestCodeGeneratorExport(const char* name, std::wstring_view code, std::wstring_view cppCode, int expectedErrors = 0);
+	static std::unique_ptr<Node> TestParser(const char* name, std::wstring_view code, const Node& expectedTree, const std::vector<IParser::Error>& expectedErrors = {});
+	static void TestCodeGenerator(const char* name, std::wstring_view code, std::wstring_view cppCode, int expectedErrors = 0);
+	static void TestCodeGeneratorImport(const char* name, std::wstring_view code, std::wstring_view cppCode, int expectedErrors = 0);
+	static void TestCodeGeneratorExport(const char* name, std::wstring_view code, std::wstring_view cppCode, int expectedErrors = 0);
 
-	void PrintTree(const Node& tree) { int i{}; PrintTree(i, tree, {}); }
+	static void PrintTree(const Node& tree) { int i{}; PrintTree(i, tree, {}); }
 
 private:
-	bool TestAST(int& index, const char* name, const Node& tree, const Node& expectedTree);
-	void PrintTree(int& index, const Node& tree, std::wstring indent);
+	static bool TestAST(int& index, const char* name, const Node& tree, const Node& expectedTree);
+	static void PrintTree(int& index, const Node& tree, std::wstring indent);
 
 	template <typename Code>
 	static std::wstring GetIncorrectErrorCodeMessage(const char* phase, const char* name, int index, Code actual, Code expected)
