5a99634d41146b73dfecdc5236087eb24a212720
Love Florgård <love_florgard@hotmail.com>
2022-09-05 22:27:58 +0200

    Move tests.

---
+++ tests/TargetsCppFunctionCalls.cpp
@@ -0,0 +1,29 @@
+#include "pch.h"
+#include "../targets/cpp/source/lovela.h"
+
+auto f_ReturnInput(lovela::context& context, const auto& in)
+{
+	static_cast<void>(context);
+	auto& v1 = in; static_cast<void>(v1);
+	return v1;
+}
+
+auto f_ReturnInputIncremented(lovela::context& context, const auto& in)
+{
+	static_cast<void>(context);
+	auto& v1 = in; static_cast<void>(v1);
+	auto v2 = v1 + 1;
+	return v2;
+}
+
+suite FunctionCalls = [] {
+	"ReturnInput"_test = [] {
+		lovela::context context;
+		expect(f_ReturnInput(context, 100) == 100);
+	};
+
+	"ReturnInputIncremented"_test = [] {
+		lovela::context context;
+		expect(f_ReturnInputIncremented(context, 100) == 101);
+	};
+};
