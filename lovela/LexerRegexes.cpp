5f5f40d3a7785738ca0420b3621eafdc24c4ba0b
Love Florgård <love_florgard@hotmail.com>
2022-11-24 19:52:44 +0100

    Move implementation to code file.

---
+++ lovela/LexerRegexes.cpp
@@ -0,0 +1,58 @@
+#include "pch.h"
+#include "LexerRegexes.h"
+
+// https://stackoverflow.com/questions/399078/what-special-characters-must-be-escaped-in-regular-expressions
+
+const std::regex& LexerRegexes::GetSeparatorRegex()
+{
+	static const std::regex re{ R"([()[\]{}.,:;!?/|#])" };
+	return re;
+}
+
+const std::regex& LexerRegexes::GetWhitespaceRegex()
+{
+	static const std::regex re{ R"(\s)" };
+	return re;
+}
+
+const std::regex& LexerRegexes::GetBeginLiteralNumberRegex()
+{
+	static const std::regex re{ R"((\d\d?|[+\-]\d|\d\.))" };
+	return re;
+}
+
+const std::regex& LexerRegexes::GetBeginCommentRegex()
+{
+	static const std::regex re{ R"(<<)" };
+	return re;
+}
+
+const std::regex& LexerRegexes::GetEndCommentRegex()
+{
+	static const std::regex re{ R"(>>)" };
+	return re;
+}
+
+const std::regex& LexerRegexes::GetDigitRegex()
+{
+	static const std::regex re{ R"(\d)" };
+	return re;
+}
+
+const std::regex& LexerRegexes::GetStringFieldRegex()
+{
+	static const std::regex re{ R"([tnr])" };
+	return re;
+}
+
+const std::regex& LexerRegexes::GetBeginDecimalPartRegex()
+{
+	static const std::regex re{ R"(\.\d)" };
+	return re;
+}
+
+const std::regex& LexerRegexes::GetBeginDecimalExponentRegex()
+{
+	static const std::regex re{ R"([eE][+\-\d])" };
+	return re;
+}
--- lovela/LexerRegexes.h
+++ lovela/LexerRegexes.h
@@ -2,60 +2,15 @@
 
 class LexerRegexes
 {
-	// https://stackoverflow.com/questions/399078/what-special-characters-must-be-escaped-in-regular-expressions
-
 public:
-	static const std::regex& GetSeparatorRegex()
-	{
-		static const std::regex re{ R"([()[\]{}.,:;!?/|#])" };
-		return re;
-	}
+	static const std::regex& GetSeparatorRegex();
+	static const std::regex& GetWhitespaceRegex();
 
-	static const std::regex& GetWhitespaceRegex()
-	{
-		static const std::regex re{ R"(\s)" };
-		return re;
-	}
-
-	static const std::regex& GetBeginLiteralNumberRegex()
-	{
-		static const std::regex re{ R"((\d\d?|[+\-]\d|\d\.))" };
-		return re;
-	}
-
-	static const std::regex& GetBeginCommentRegex()
-	{
-		static const std::regex re{ R"(<<)" };
-		return re;
-	}
-
-	static const std::regex& GetEndCommentRegex()
-	{
-		static const std::regex re{ R"(>>)" };
-		return re;
-	}
-
-	static const std::regex& GetDigitRegex()
-	{
-		static const std::regex re{ R"(\d)" };
-		return re;
-	}
-
-	static const std::regex& GetStringFieldRegex()
-	{
-		static const std::regex re{ R"([tnr])" };
-		return re;
-	}
-
-	static const std::regex& GetBeginDecimalPartRegex()
-	{
-		static const std::regex re{ R"(\.\d)" };
-		return re;
-	}
-
-	static const std::regex& GetBeginDecimalExponentRegex()
-	{
-		static const std::regex re{ R"([eE][+\-\d])" };
-		return re;
-	}
+	static const std::regex& GetBeginCommentRegex();
+	static const std::regex& GetEndCommentRegex();
+	static const std::regex& GetBeginLiteralNumberRegex();
+	static const std::regex& GetDigitRegex();
+	static const std::regex& GetBeginDecimalPartRegex();
+	static const std::regex& GetBeginDecimalExponentRegex();
+	static const std::regex& GetStringFieldRegex();
 };
