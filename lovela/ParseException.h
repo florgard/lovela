91e5c70b3ae75b0aa5ab5c67f6b673100096a6b1
Love Florgård <love_florgard@hotmail.com>
2022-11-27 17:57:10 +0100

    Inherit std::exception.

--- lovela/ParseException.h
+++ lovela/ParseException.h
@@ -1,13 +1,18 @@
 #pragma once
 #include "Token.h"
 
-struct ParseException
+struct ParseException : public std::exception
 {
 	std::string message;
 	Token token;
 
 	ParseException(const Token& token) noexcept;
 	ParseException(const Token& token, std::string_view message) noexcept;
+
+	const char* what() const noexcept override
+	{
+		return message.c_str();
+	}
 };
 
 struct UnexpectedTokenException : public ParseException
