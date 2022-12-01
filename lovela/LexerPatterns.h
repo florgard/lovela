479b83aa44e3044f8d22a681471bdd4516a992ad
Love Florgård <love_florgard@hotmail.com>
2022-12-01 20:19:15 +0100

    Simplify initialization.

--- lovela/LexerPatterns.h
+++ lovela/LexerPatterns.h
@@ -12,22 +12,27 @@ struct LexerPatterns
 	struct Regex
 	{
 		static_assert(length >= 1 && length <= 2, "The number of characters to match must be 1 or 2");
+
 		const std::regex regex;
+
+		constexpr Regex(const char* re) noexcept : regex(re)
+		{
+		}
 	};
 
 	// https://stackoverflow.com/questions/399078/what-special-characters-must-be-escaped-in-regular-expressions
 
-	const Regex<1> whitespace{ std::regex(R"(\s)") };
-	const Regex<1> separator{ std::regex(R"([()[\]{}.,:;!?/|#])") };
-	const Regex<1> digit{ std::regex(R"(\d)") };
+	const Regex<1> whitespace{ R"(\s)" };
+	const Regex<1> separator{ R"([()[\]{}.,:;!?/|#])" };
+	const Regex<1> digit{ R"(\d)" };
 
 	const Chars beginComment{ '<', '<' };
 	const Chars endComment{ '>', '>' };
 
-	const Regex<2> beginLiteralNumber{ std::regex(R"((\d.|[+\-]\d))") };
-	const Regex<2> beginDecimalPart{ std::regex(R"(\.\d)") };
-	const Regex<2> beginDecimalExponent{ std::regex(R"([eE][+\-\d])") };
+	const Regex<2> beginLiteralNumber{ R"((\d.|[+\-]\d))" };
+	const Regex<2> beginDecimalPart{ R"(\.\d)" };
+	const Regex<2> beginDecimalExponent{ R"([eE][+\-\d])" };
 
 	const char beginString{ '\'' };
-	const Regex<1> stringField{ std::regex(R"([tnr])") };
+	const Regex<1> stringField{ R"([tnr])" };
 };
