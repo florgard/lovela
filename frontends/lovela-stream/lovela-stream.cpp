e6b677dea501284d2555b88a568b2d9f970d2da1
Love Florgård <love_florgard@hotmail.com>
2022-12-06 22:03:07 +0100

    Use stream API.

--- frontends/lovela-stream/lovela-stream.cpp
+++ frontends/lovela-stream/lovela-stream.cpp
@@ -8,8 +8,8 @@ int main_utf8(int argc, char** argv)
 {
 	std::unique_ptr<StreamLexer> lexer;
 	RangeParser parser;
-	CodeGeneratorCpp coder(std::cout);
-	std::cin >> lexer >> parser >> coder;
+	CodeGeneratorCpp coder;
+	std::cin >> lexer >> parser >> coder >> std::cout;
 
 	return 0;
 }
